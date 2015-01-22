e4c1e8194f8494c2fde01e0d28d24c4996450741
xLeachimx <michael.huelsman@gmail.com>
2015-01-21 22:25:17 -0500

    finsihed fleshing out

--- semantic_network/semantic_net.rb
+++ semantic_network/semantic_net.rb
@@ -10,15 +10,26 @@ class SemanticNet
 	attr_accessor :nodes
 
 	def initialize
-		nodes = Array.new()
+		@nodes = Array.new()
 	end
 
 	def addNode name
+		if @nodes.length == 0
+			@nodes.push(SemanticNode.new(name))
+			return
+		end
+		for i in 0...@nodes.length
+			if name < @nodes[i].name
+				@nodes.insert(i,SemanticNode.new(name))
+				return
+		end
 	end
 
 	def addConnection from, to, type
+		getNode(from).addConnection(getNode(type),type)
 	end
 
 	def getNode name
+		@nodes.bsearch{|x| x.name == name}.clone
 	end
 end
